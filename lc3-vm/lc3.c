a712c3b0866b709c4f320ba648b55977b25bd49b
Suhaas Joshi <joshiesuhaas0@gmail.com>
2023-03-04 10:20:45 +0530

    lc3-vm: write skeletal main() function
    This lays the foundation of the VM and gives a structure and direction
    for implementing the details.
    
    The code doesn't compile yet since some functions that are called
    have not been implemented.

--- lc3-vm/lc3.c
+++ lc3-vm/lc3.c
@@ -20,7 +20,7 @@ enum {
 	FL_NEG = 1 << 2;
 };
 
-uint16_t registers[R_COUNT];
+uint16_t regs[R_COUNT];
 
 enum {
 	OP_BR = 0,
@@ -40,3 +40,40 @@ enum {
 	OP_LEA,
 	OP_TRAP,
 };
+
+int main(int argc, char **args)
+{
+	if (argc < 2) {
+		printf("Specify input file name.\n");
+		exit(1);
+	}
+
+	for (int i = 0; i < argc; ++i) {
+		if (!read_file(args[i])) {
+			printf("Could not read file %s\n. Please ensure it exists.\n", args[i]);
+			exit(1);
+		} else {
+			printf("Successfully loaded file %s\n.", args[i]);
+		}
+	}
+
+	regs[R_COND] = FL_ZRO;
+	regs[R_PC] = 0x3000;
+
+	while (1) {
+		uint16_t instr = mem_read(regs[R_PC]++);
+		uint16_t opcode = instr >> 12;
+
+		switch (opcode) {
+			case OP_ADD:
+				break;
+			case OP_AND:
+				break;
+			case OP_NOT:
+				break;
+			default:
+				printf("Bad instruction\n");
+				break;
+		}
+	}
+}
