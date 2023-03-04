31b097949350e79a92133424f01a1fac23c9cac5
Suhaas Joshi <joshiesuhaas0@gmail.com>
2023-03-04 16:06:32 +0530

    lc3-vm: implement a bunch of operations PART 1
    These operations include:
    	- and
    	- load
    	- load register
    	- break
    	- jump
    	- jump to subroutine

--- lc3-vm/operations.c
+++ lc3-vm/operations.c
@@ -25,3 +25,54 @@ void op_ldi(uint16_t instr)
 	regs[dst_r] = mem_read(mem_read(regs[R_PC] + pc_offset));
 	update_flag(dst_r);
 }
+
+void op_and(uint16_t instr)
+{
+	uint16_t dst_r = (instr >> 9) & 0x07;
+	uint16_t src_r = (instr >> 6) & 0x07;
+	if ((instr >> 5) & 0x01) {
+		uint16_t imm = sign_extend(instr & 0x1F, 5);
+		regs[dst_r] = regs[src_r] & imm;
+	} else {
+		uint16_t src2_r = instr & 0x07;
+		regs[dst_r] = regs[src_r] & src2_r;
+	}
+	update_flags(dst_r);
+}
+
+void op_br(uint16_t instr)
+{
+	uint16_t pc_offset = sign_extend(instr & 0x1FF, 9);
+	uint16_t cond = (instr >> 9) & 0x07;
+	if (cond & regs[R_COND])
+		regs[PC] += pc_offset;
+}
+
+void op_jmp(uint16_t instr)
+{
+	regs[PC] = (instr >> 6) & 0x07;
+}
+
+void op_jsr(uint16_t instr)
+{
+	regs[R_R7] = regs[R_PC];
+	/* JSR or JSRR */
+	regs[R_PC] = ((instr >> 6) & 0x01) ? regs[R_PC] + sign_extend(instr >> 0x7FF, 11) : (instr >> 6) & 0x07;
+}
+
+void op_ld(uint16_t instr)
+{
+	uint16_t dst_r = (instr >> 9) & 0x07;
+	uint16_t pc_offset = sign_extend(instr & 0x1FF, 9);
+	regs[dst_r] = mem_read(regs[R_PC] + pc_offset);
+	update_flag(dst_r);
+}
+
+void op_ldr(uint16_t instr)
+{
+	uint16_t dst_r = (instr >> 9) & 0x07;
+	uint16_t base_r = (instr >> 6) & 0x07;
+	uint16_t pc_offset = sign_extend(instr & 0x3F, 5);
+	regs[dst_r] = mem_read(regs[base_r] + pc_offset);
+	update_flag(dst_r);
+}
--- lc3-vm/operations.h
+++ lc3-vm/operations.h
@@ -3,5 +3,11 @@
 
 void op_add(uint16_t instr);
 void op_ldi(uint16_t instr);
+void op_and(uint16_t instr);
+void op_br(uint16_t instr);
+void op_jmp(uint16_t instr);
+void op_jsr(uint16_t instr);
+void op_ld(uint16_t instr);
+void op_ldr(uint16_t instr);
 
 #endif // OPERATIONS
