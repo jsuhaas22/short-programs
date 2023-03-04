a15de950daf45fcc114c56d8e9f8c32369bfd20a
Suhaas Joshi <joshiesuhaas0@gmail.com>
2023-03-04 11:42:42 +0530

    lc3-vm: create a "global" file
    The file contains all enums and global variables such as the array
    "reg[]". This is necessary so that the file implementing operations can
    access registers without complicating parameter lists and dealing with
    the slight overhead of parameter passing.

---
+++ lc3-vm/global.h
@@ -0,0 +1,45 @@
+#ifndef GLOBAL
+#define GLOBAL
+
+enum {
+	R_R0 = 0,
+	R_R1,
+	R_R2,
+	R_R3,
+	R_R4,
+	R_R5,
+	R_R6,
+	R_R7,
+	R_PC,
+	R_COND,
+	R_COUNT,
+};
+
+enum {
+	FL_POS = 1 << 0;
+	FL_ZRO = 1 << 1;
+	FL_NEG = 1 << 2;
+};
+
+enum {
+	OP_BR = 0,
+	OP_ADD,
+	OP_LD,
+	OP_ST,
+	OP_JSR,
+	OP_AND,
+	OP_LDR,
+	OP_STR,
+	OP_RTI,
+	OP_NOT,
+	OP_LDI,
+	OP_STI,
+	OP_JMP,
+	OP_RES,
+	OP_LEA,
+	OP_TRAP,
+};
+
+extern uint16_t regs[R_COUNT];
+
+#endif // GLOBAL
--- lc3-vm/lc3.c
+++ lc3-vm/lc3.c
@@ -1,45 +1,5 @@
+#include "global.h"
 #include <stdio.h>
-
-enum {
-	R_R0 = 0,
-	R_R1,
-	R_R2,
-	R_R3,
-	R_R4,
-	R_R5,
-	R_R6,
-	R_R7,
-	R_PC,
-	R_COND,
-	R_COUNT,
-};
-
-enum {
-	FL_POS = 1 << 0;
-	FL_ZRO = 1 << 1;
-	FL_NEG = 1 << 2;
-};
-
-uint16_t regs[R_COUNT];
-
-enum {
-	OP_BR = 0,
-	OP_ADD,
-	OP_LD,
-	OP_ST,
-	OP_JSR,
-	OP_AND,
-	OP_LDR,
-	OP_STR,
-	OP_RTI,
-	OP_NOT,
-	OP_LDI,
-	OP_STI,
-	OP_JMP,
-	OP_RES,
-	OP_LEA,
-	OP_TRAP,
-};
 
 int main(int argc, char **args)
 {
@@ -56,6 +16,8 @@ int main(int argc, char **args)
 			printf("Successfully loaded file %s\n.", args[i]);
 		}
 	}
+
+	uint16_t regs[R_COUNT];
 
 	regs[R_COND] = FL_ZRO;
 	regs[R_PC] = 0x3000;
