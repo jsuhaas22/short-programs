9422b902793465b9a1d84609d1975a38218cf0f6
Suhaas Joshi <joshiesuhaas0@gmail.com>
2023-03-04 10:07:34 +0530

    lc3-vm: create enums for registers, flags and opcodes

---
+++ lc3-vm/lc3.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+enum {
+	R_R0 = 0,
+	R_R1,
+	R_R2,
+	R_R3,
+	R_R4,
+	R_R5,
+	R_R6,
+	R_R7,
+	R_PC,
+	R_COND,
+	R_COUNT,
+};
+
+enum {
+	FL_POS = 1 << 0;
+	FL_ZRO = 1 << 1;
+	FL_NEG = 1 << 2;
+};
+
+uint16_t registers[R_COUNT];
+
+enum {
+	OP_BR = 0,
+	OP_ADD,
+	OP_LD,
+	OP_ST,
+	OP_JSR,
+	OP_AND,
+	OP_LDR,
+	OP_STR,
+	OP_RTI,
+	OP_NOT,
+	OP_LDI,
+	OP_STI,
+	OP_JMP,
+	OP_RES,
+	OP_LEA,
+	OP_TRAP,
+};
