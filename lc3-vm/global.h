d0040a471646e177954d042d71c78d58b271ef3b
Suhaas Joshi <joshiesuhaas0@gmail.com>
2023-03-05 12:33:43 +0530

    lc3-vm: specify hexadecimal codes with trap codes

--- lc3-vm/global.h
+++ lc3-vm/global.h
@@ -39,18 +39,18 @@ enum {
 	OP_JMP,
 	OP_RES,
 	OP_LEA,
-	OP_TRAP,
+	OP_TRAP
 };
 
 extern uint16_t regs[R_COUNT];
 
 enum {
 	TRAP_GETC = 0x20,
-	TRAP_OUT,
-	TRAP_IN,
-	TRAP_PUTS,
-	TRAP_PUTSP,
-	TRAP_HALT,
+	TRAP_OUT = 0x21,
+	TRAP_PUTS = 0x22,
+	TRAP_IN = 0x23,
+	TRAP_PUTSP = 0x24,
+	TRAP_HALT = 0x25,
 };
 
 #define MEMORY_SIZE (1 << 16)
